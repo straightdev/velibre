409320601caaf8c2ff7c83fb337da2f09d670765
Anca Spatariu <anca.spatariu@gmail.com>
2016-03-28 11:46:36 +0300

    Make hero headlines responsive with fluid font faces

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 (function($){
   $(document).ready(function(){
-    // ...
+
+    // Fluid font face for the hero screen
+    $("#responsive-headline1").fitText(1.5, { minFontSize: '40px', maxFontSize: '90px' });
+    $("#responsive-headline2").fitText(2, { minFontSize: '25px', maxFontSize: '50px' });
+
   });
 })(jQuery);
