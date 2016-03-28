02811187761be77aede2e14c75efa5c123d526af
Anca Spatariu <anca.spatariu@gmail.com>
2016-03-28 15:15:00 +0300

    Add smooth scrolling between sections (arrows).

--- js/main.js
+++ js/main.js
@@ -5,5 +5,16 @@
     $("#responsive-headline1").fitText(1.5, { minFontSize: '40px', maxFontSize: '90px' });
     $("#responsive-headline2").fitText(2, { minFontSize: '25px', maxFontSize: '50px' });
 
+    $('.screen-arrow a').on('click',function (e) {
+	    e.preventDefault();
+      var target = this.hash;
+	    var $target = $(target);
+      $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 1000);
+
+	});
+
+
   });
 })(jQuery);
