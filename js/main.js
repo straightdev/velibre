0cb735f7d78f537eae2941602865a2e474c6886a
Anca Spatariu <anca.spatariu@gmail.com>
2016-03-31 14:10:40 +0300

    JavaScript resize bug fixed. Submitted to testing.

--- js/main.js
+++ js/main.js
@@ -98,45 +98,72 @@
 
     // Snap pages
 
-    if (window.innerWidth >= 1200) {
+var screenOK = window.innerWidth >= 1200;
+console.log(screenOK)
+
+function sectionScoll (OK) {
+    console.log ("parameter is = " + OK);
+    if(!OK){
+      $('.snap').each(function() {
+        $(this).off('mousewheel DOMMouseScroll');
+      });
+    } else {
+
+      console.log ("running function with OK = " + OK)
       var snapPositions = [];
 
       $('.snap').each(function() {
       snapPositions.push(parseInt($(this).offset().top));
       $(this).on({
-        'mousewheel DOMMouseScroll': function(e) {
+          'mousewheel DOMMouseScroll': function(e) {
 
-          // disable the scrolling event inside this element
-          e.preventDefault();
-          e.stopPropagation();
+            // disable the scrolling event inside this element
+            e.preventDefault();
+            e.stopPropagation();
 
-          // snap to previous or to next element
-          var thisPosition = parseInt($(this).offset().top);
-          var snapPosition = 0;
-          var delta = 0;
+            // snap to previous or to next element
+            var thisPosition = parseInt($(this).offset().top);
+            var snapPosition = 0;
+            var delta = 0;
 
-          if (e.type == 'mousewheel') {
-            delta = e.originalEvent.wheelDelta;
-          } else if (e.type == 'DOMMouseScroll') {
-            delta = e.originalEvent.detail;
+            if (e.type == 'mousewheel') {
+              delta = e.originalEvent.wheelDelta;
+            } else if (e.type == 'DOMMouseScroll') {
+              delta = e.originalEvent.detail;
+            }
+            if (delta >= 0) {
+              snapPosition = snapPositions[snapPositions.indexOf(thisPosition)-1];
+            }
+            else {
+              snapPosition = snapPositions[snapPositions.indexOf(thisPosition)+1];
+            }
+            snapPosition = snapPosition - 115; // decrease number to include top arrow
+            if (snapPosition !== "undefined")  {
+              $('html, body').stop().animate({
+                'scrollTop': snapPosition
+              }, 100);
+            }
           }
-          if (delta >= 0) {
-            snapPosition = snapPositions[snapPositions.indexOf(thisPosition)-1];
-          }
-          else {
-            snapPosition = snapPositions[snapPositions.indexOf(thisPosition)+1];
-          }
-          snapPosition = snapPosition - 115; // decrease number to include top arrow
-          if (snapPosition !== "undefined") {
-            $('html, body').stop().animate({
-              'scrollTop': snapPosition
-            }, 100);
-          }
-        }
       });
 
     });
-    }
+  }
+} //end of scroll function
+
+    sectionScoll(screenOK);
+
+  $(window).resize(function() {
+    if (window.innerWidth < 1200) {
+      screenOK = false;
+      console.log (screenOK);
+      sectionScoll(screenOK);
+  } else if (window.innerWidth >= 1200) {
+    screenOK = true;
+    console.log (screenOK);
+    sectionScoll(screenOK);
+  }
+});
+
 
 
 
